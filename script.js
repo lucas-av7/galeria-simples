11ac1b95d83f491b64c6971f357bf69a839e36a0
LUCAS VASCONCELOS <lucas.av7@yahoo.com>
2020-05-14 14:48:15 -0400

    Galeria simples modelo 2

--- script.js
+++ script.js
@@ -18,3 +18,40 @@ function setImg(e) {
 }
 
 // Galeria 2
+
+let imgFull2 = document.querySelector('.imgFull2');
+let setaDir = document.querySelector('.setaDir');
+let setaEsq = document.querySelector('.setaEsq');
+
+let qtdFotos2 = 10;
+let fotoAtual  = 1;
+
+setaDir.addEventListener('click', mudarFoto);
+setaEsq.addEventListener('click', mudarFoto);
+
+function mudarFoto(e) {
+
+    console.log(e);
+    let direcao = e.target.getAttribute('class');
+    
+    if(fotoAtual === qtdFotos2 && direcao === 'setas setaDir'
+     || fotoAtual === 1 && direcao === 'setas setaEsq') {
+        return;
+    } else {
+        switch(direcao) {
+            case 'setas setaDir':
+                imgFull2.setAttribute('src', `images/pic${fotoAtual+1}.jpg`)
+                fotoAtual++;
+                break;
+
+            case 'setas setaEsq':
+                imgFull2.setAttribute('src', `images/pic${fotoAtual-1}.jpg`)
+                fotoAtual--;
+                break;
+
+            default:
+                return;
+        }
+    }
+    
+}
